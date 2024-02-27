cef5c2487063c18484cad0aaecc1292df480e844
CodePawsDev <isripen1991@outlook.com>
2024-02-27 13:49:47 +0700

    feat: add adjust HH exercises

---
+++ function/HH/HH-function-ex1.js
@@ -0,0 +1,5 @@
+/* ให้ Declare ฟังก์ชัน addTwoNumber โดยใช้รูปแบบของ Function Declaration
+ฟังก์ชันนี้จะรับ Parameter 2 ตัวคือ firstNumber มีชนิดข้อมูลเป็น Number และ secondNumber มีชนิดข้อมูลเป็น Number
+ฟังก์ชันนี้จะ Return ผลลัพธ์ออกมาเป็น ผลรวมของ firstNumber และ secondNumber
+ถ้า Execute addTwoNumber(10, 5) จะได้ผลลัพธ์ออกมาเป็น 15
+*/
---
+++ function/HH/HH-function-ex4.js
@@ -0,0 +1,21 @@
+/* ให้ Declare ตัว Object ที่ชื่อ calculator เพื่อรวบรวมฟังก์ชันสองอัน 
+คือ addTwoNumber และ subtractTwoNumber ให้อยู่ใน Object โดยที่
+Object calculator จะมี 
+1. Key เป็น addTwoNumber และมีค่าเป็นฟังก์ชัน addTwoNumber
+2. Key เป็น subtractTwoNumber และมีค่าเป็นฟังก์ชัน subtractTwoNumber
+
+สำหรับฟังก์ชัน addTwoNumber สามารถนำ code ในข้อ 1 มาใช้ได้
+
+ส่วนฟังก์ชัน subtractTwoNumber ให้ declare ใหม่โดยใช้รูปแบบ Function Declaration
+ฟังก์ชันนี้จะรับ Parameter 2 ตัวคือ firstNumber มีชนิดข้อมูลเป็น Number และ secondNumber มีชนิดข้อมูลเป็น Number
+ฟังก์ชันนี้จะ Return ผลลัพธ์ออกมาเป็น ผลต่างของ firstNumber และ secondNumber
+เช่น ถ้า Execute subtractTwoNumber(10, 5) จะได้ผลลัพธ์ออกมาเป็น 5
+
+ต่อมาให้ Declare ตัว Variable ที่มีชื่อว่า addResult และ Assign ค่าของ addResult โดยมี Value เป็นผลลัพธ์จากการ Execute ฟังก์ชัน addTwoNumber ที่อยู่ใน Object calculator 
+ให้กำหนด Parameter firstNumber เป็น 10 และ secondNumber เป็น 20
+ให้แสดงค่าของ addResult ออกมาดูทางหน้าจอด้วย console.log ถ้าถูกต้องจะเห็น 30
+
+สุดท้าย ให้ Declare ตัว Variable ที่มีชื่อว่า subtractResult และ Assign ค่าของ subtractResult โดยที่ Value เป็นผลลัพธ์จากการ Execute ฟังก์ชัน subtractTwoNumber ที่อยู่ใน Object calculator
+ให้กำหนด Parameter firstNumber เป็น 3000 และ secondNumber เป็น 1000
+ให้แสดงค่าของ subtractResult ออกมาดูทางหน้าจอด้วย console.log ถ้าถูกต้องจะเห็น 2000
+*/
